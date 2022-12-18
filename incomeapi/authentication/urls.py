97f10cc5fff2b71637b4cffe5da90e290f22a469
John <johnmuchirim@gmail.com>
2022-12-18 11:33:59 +0300

    Initial commit

---
+++ incomeapi/authentication/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from .views import RegisterView, VerifyEmail, LoginAPIView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name="register"),
+    path('login/', LoginAPIView.as_view(), name="login"),
+    path('email-verify/', VerifyEmail.as_view(), name="email-verify"),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
+
+
+
+]
